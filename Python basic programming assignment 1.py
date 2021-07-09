c252e48aac8f0858d2e2be2f680167b5fdff29a1
Chelsea Rodrigues <84131870+chelsea2311@users.noreply.github.com>
2021-07-09 11:43:17 +0530

    Add files via upload

---
+++ Python basic programming assignment 1.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#Q1.
+print("Hello World")
+
+
+# In[27]:
+
+
+#Q2.
+a = 20
+b = 10
+c = a + b
+d = a / b
+print("addition of a and b is {}".format(c))
+print("division of a and b is {}".format(d))
+
+
+# In[28]:
+
+
+#Q3.
+base = 30
+height = 4
+Area = 0.5 * base * height
+print("Area of Triangle is {}".format(Area))
+
+
+# In[26]:
+
+
+#Q4.
+a = 5
+b = 10
+var = a
+a = b
+b = var
+print ("The value of a after swapping is {}".format(a))
+print ("The value of b after swapping is {}".format(b))
+
+
+# In[30]:
+
+
+#Q5.
+import random
+c = random.random()
+print(c)
+
+
+# In[ ]:
+
+
+
+
